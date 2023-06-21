e7005a704cd5d2593c15ea1d07044056e6e85b30
mayur1377 <mayuresh1377@gmail.com>
2023-06-21 18:15:56 +0530

    changes with name ans usn

--- temp_V2.py
+++ temp_V2.py
@@ -1,16 +1,17 @@
 import logging
 import os
+import re
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
 
-# Enable logging
+# Enable loggingṇ
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 # Define the states for the conversation
-BRANCH, SEMESTER, SUBJECT, UNIT , BRANCH2 = range(5)
+NAME, USN, SEMESTER, SUBJECT, UNIT = range(5)
 
 # Define the subjects for each branch and semester
 subjects = {
@@ -28,65 +29,65 @@ subjects = {
 
 # Handler for the /start command
 def start(update, context):
-    branch_buttons = [
-        [InlineKeyboardButton("CS", callback_data="CS")],
-        [InlineKeyboardButton("IS", callback_data="IS")]
-    ]
-    reply_markup = InlineKeyboardMarkup(branch_buttons)
+    user_id = str(update.effective_user.id)
+    context.user_data[user_id] = {}
 
-    update.message.reply_text('Welcome! Please select your branch:', reply_markup=reply_markup)
+    update.message.reply_text('Welcome! Please enter your name:')
+    return NAME
 
-    return BRANCH
+def collect_name(update, context):
+    user_id = str(update.effective_user.id)
+    context.user_data[user_id]['name'] = update.message.text
+    
+    update.message.reply_text('Please enter your USN (University Seat Number):')
+    
+    return USN
 
-# Handler for the branch selection
-def select_branch2(update, context):
-    query = update.callback_query
-    context.user_data['branch'] = query.data
 
-    semester_buttons = [
-        [InlineKeyboardButton("1", callback_data="1"),
-         InlineKeyboardButton("2", callback_data="2")]
-        # Add more semesters if needed
-    ]
-    reply_markup = InlineKeyboardMarkup(semester_buttons)
-
-    query.edit_message_text(text=f'You selected {context.user_data["branch"]}. Please select your semester:',
+# Handler for validating the USN format
+def validate_usn(update, context):
+    user_id = str(update.effective_user.id)
+    usn = update.message.text.upper()  # Convert to uppercase for case insensitivity
+    query = update.callback_query    
+    # Define the regular expression pattern for the USN format
+    pattern = r'^\dSI\d{2}(CS|IS|AI|EC|EE)\d{3}$'
+    
+    if re.match(pattern, usn):
+        context.user_data[user_id]['usn'] = usn
+        branch_from_usn = usn[5:7]
+        print(branch_from_usn)
+        context.user_data[user_id]['branch'] = branch_from_usn
+        semester_buttons = [
+            [InlineKeyboardButton("1", callback_data="1"),
+            InlineKeyboardButton("2", callback_data="2")]
+            # Add more semesters if needed
+        ]
+        reply_markup = InlineKeyboardMarkup(semester_buttons)
+        update.effective_message.reply_text(text=f'You selected {context.user_data[user_id]["branch"]}. Please select your semester:',
                             reply_markup=reply_markup)
 
-    return SEMESTER
-
-def select_branch(update, context):
-    query = update.callback_query
-    context.user_data['branch'] = query.data
-
-    semester_buttons = [
-        [InlineKeyboardButton("1", callback_data="1"),
-         InlineKeyboardButton("2", callback_data="2")]
-        # Add more semesters if needed
-    ]
-    reply_markup = InlineKeyboardMarkup(semester_buttons)
-
-    query.edit_message_text(text=f'You selected {context.user_data["branch"]}. Please select your semester:',
-                            reply_markup=reply_markup)
-
-    return SEMESTER
+        return SEMESTER
+    else:
+        update.message.reply_text('Invalid USN format! Please try again.')
+        return USN
 
 # Handler for the semester selection
 def select_semester(update, context):
     query = update.callback_query
-    context.user_data['semester'] = query.data
+    user_id = str(query.from_user.id)
+    context.user_data[user_id]['semester'] = query.data
 
     subject_buttons = []
-    branch = context.user_data['branch']
-    semester = context.user_data['semester']
+    branch = context.user_data[user_id]['branch']
+    semester = context.user_data[user_id]['semester']
     if branch in subjects and semester in subjects[branch]:
         semester_subjects = subjects[branch][semester]
         for subject in semester_subjects:
             subject_buttons.append([InlineKeyboardButton(subject, callback_data=subject)])
 
-    reply_markup = InlineKeyboardMarkup(subject_buttons)
+    reply_markup = InlineKeyboardMarkup(subject_buttons + [[InlineKeyboardButton("Go Back", callback_data="go_back")]])
 
-    query.edit_message_text(text=f'You selected {context.user_data["branch"]} branch and semester {context.user_data["semester"]}. '
+    query.edit_message_text(text=f'You selected {context.user_data[user_id]["branch"]} branch and semester {context.user_data[user_id]["semester"]}. '
                                  f'Please select a subject:',
                             reply_markup=reply_markup)
 
@@ -95,18 +96,31 @@ def select_semester(update, context):
 # Handler for subject selection
 def select_subject(update, context):
     query = update.callback_query
-    context.user_data['subject'] = query.data
+    user_id = str(query.from_user.id)
+    if query.data == 'go_back':
+        semester_buttons = [
+            [InlineKeyboardButton("1", callback_data="1"),
+             InlineKeyboardButton("2", callback_data="2")]
+        # Add more semesters if needed
+        ]
+        reply_markup = InlineKeyboardMarkup(semester_buttons)
+        query.edit_message_text(text=f'You selected {context.user_data[user_id]["branch"]}. Please select your semester:',
+                            reply_markup=reply_markup)
+        return SEMESTER
+
+    context.user_data[user_id]['subject'] = query.data
 
     unit_buttons = [
         [InlineKeyboardButton("Unit 1", callback_data="unit_1"),
          InlineKeyboardButton("Unit 2", callback_data="unit_2")],
         [InlineKeyboardButton("Unit 3", callback_data="unit_3"),
          InlineKeyboardButton("Unit 4", callback_data="unit_4"),
-         InlineKeyboardButton("Unit 5", callback_data="unit_5")]
+         InlineKeyboardButton("Unit 5", callback_data="unit_5")],
+        [InlineKeyboardButton("Go Back", callback_data="go_back_subject")]
     ]
     reply_markup = InlineKeyboardMarkup(unit_buttons)
 
-    query.edit_message_text(text=f'You selected {context.user_data["subject"]}. '
+    query.edit_message_text(text=f'You selected {context.user_data[user_id]["subject"]}. '
                                  f'Please select the unit:',
                             reply_markup=reply_markup)
 
@@ -115,12 +129,27 @@ def select_subject(update, context):
 # Handler for unit selection
 def select_unit(update, context):
     query = update.callback_query
-    context.user_data['unit'] = query.data
+    user_id = str(query.from_user.id)
+    if query.data == 'go_back_subject':
+        subject_buttons = []
+        branch = context.user_data[user_id]['branch']
+        semester = context.user_data[user_id]['semester']
+        if branch in subjects and semester in subjects[branch]:
+            semester_subjects = subjects[branch][semester]
+            for subject in semester_subjects:
+                subject_buttons.append([InlineKeyboardButton(subject, callback_data=subject)])
 
-    branch = context.user_data['branch']
-    semester = context.user_data['semester']
-    subject = context.user_data['subject']
-    unit = context.user_data['unit']
+        reply_markup = InlineKeyboardMarkup(subject_buttons + [[InlineKeyboardButton("Go Back", callback_data="go_back")]])
+        query.edit_message_text(text=f'You selected {context.user_data[user_id]["branch"]} branch and semester {context.user_data[user_id]["semester"]}. '
+                                 f'Please select a subject:',
+                            reply_markup=reply_markup)
+
+        return SUBJECT
+    context.user_data[user_id]['unit'] = query.data
+    branch = context.user_data[user_id]['branch']
+    semester = context.user_data[user_id]['semester']
+    subject = context.user_data[user_id]['subject']
+    unit = context.user_data[user_id]['unit']
 
     file_name = f'{branch}_{semester}_{subject}_{unit}.pdf'
     file_path = os.path.join(os.path.dirname(__file__), file_name)
@@ -132,50 +161,70 @@ def select_unit(update, context):
     else:
         update.effective_message.reply_text('PDF file not found.')
 
-    # Reset the conversation state
-    context.user_data.clear()
-    branch_buttons = [
-        [InlineKeyboardButton("CS", callback_data="CS")],
-        [InlineKeyboardButton("IS", callback_data="IS")]
-    ]
-    reply_markup = InlineKeyboardMarkup(branch_buttons)
+    subject_buttons = []
+    branch = context.user_data[user_id]['branch']
+    semester = context.user_data[user_id]['semester']
+    if branch in subjects and semester in subjects[branch]:
+        semester_subjects = subjects[branch][semester]
+        for subject in semester_subjects:
+            subject_buttons.append([InlineKeyboardButton(subject, callback_data=subject)])
 
-    update.effective_message.reply_text(text=f'ok',
-                            reply_markup=reply_markup)
-    return BRANCH2
+    reply_markup = InlineKeyboardMarkup(subject_buttons + [[InlineKeyboardButton("Go Back", callback_data="go_back")]])
+
+    update.effective_message.reply_text(text=f'You selected {context.user_data[user_id]["branch"]} branch and semester {context.user_data[user_id]["semester"]}. '
+                                              f'Please select a subject:',
+                                        reply_markup=reply_markup)
+
+    return SUBJECT
+
+
+# Handler for unknown commands
+# def unknown(update, context):
+#     user_id = str(update.effective_user.id)
+#     if user_id in context.user_data:
+#         del context.user_data[user_id]
+#     update.message.reply_text('Unknown command! Please try again.')
 
 # Handler for canceling the conversation
 def cancel(update, context):
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text('The conversation has been canceled. Goodbye!')
-
+    update.message.reply_text('Conversation canceled.')
     return ConversationHandler.END
 
-# Main function to start the bot
+# Main function
 def main():
-    # Create the EventHandler and pass it your bot's token
-    updater = Updater("6052177187:AAFZOl3I4Ftj1iN8uRrSp92D2VitkH3aYlE", use_context=True)
+    # Get the Telegram bot token from an environment variable
+    token = '6052177187:AAFZOl3I4Ftj1iN8uRrSp92D2VitkH3aYlE'
 
-    # Get the dispatcher to register handlers
+    if token is None:
+        print('Please set the TELEGRAM_BOT_TOKEN environment variable.')
+        return
+
+    # Create the Updater and dispatcher
+    updater = Updater(token, use_context=True)
     dp = updater.dispatcher
 
-    # Add conversation handler with the states
+    # Create the conversation handler
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
+
         states={
-            BRANCH: [CallbackQueryHandler(select_branch, pattern='^(CS|IS)$')],
+            NAME: [MessageHandler(Filters.text, collect_name)],
+            USN: [MessageHandler(Filters.text, validate_usn)],
             SEMESTER: [CallbackQueryHandler(select_semester)],
             SUBJECT: [CallbackQueryHandler(select_subject)],
-            UNIT: [CallbackQueryHandler(select_unit)] , 
-            BRANCH2:[CallbackQueryHandler(select_branch2)]
-        } ,
+            UNIT: [CallbackQueryHandler(select_unit)],
+        },
+
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
+    # Add the conversation handler to the dispatcher
     dp.add_handler(conv_handler)
 
-    # Start the Bot
+    # Add handler for unknown commands
+    # dp.add_handler(MessageHandler(Filters.command, unknown))
+
+    # Start the bot
     updater.start_polling()
 
     # Run the bot until you press Ctrl-C
